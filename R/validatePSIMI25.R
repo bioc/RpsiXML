241f30c104f4d162601a7809f016f5ec3c93a9ff
David Zhang <j.zhang@dkfz.de>
2008-06-18 10:59:03 +0000

    Syntax fix: windows will warns the user not supported
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RpsiXML@32327 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/validatePSIMI25.R
+++ R/validatePSIMI25.R
@@ -16,12 +16,19 @@ validatePSIMI25 <- function(file,
 
   ## so far the routine depends on the tool (xmllint) coming together with libxml2
   ## later it may be replaced by C routines
+  if(.Platform$OS.type == "windows") {
+    if(interactive()) {
+      cat("Oops! So far only UNIX is supported!")
+    }
+    return(invisible(0))
+  }
+  
   val <- system(paste("xmllint --noout --schema", schema, file),ignore.stderr=T)
 
   if ( val == 0 ) {
     cat(paste(ofile, "valid!\n"))
   } else {
-    cat(paste(ofile, "invalid! Error number:",val,"\n"))
+    cat(paste(ofile, "invalid! Error code:",val,"\n"))
     if(!ignore.stderr) {
       system(paste("xmllint --noout --schema", schema, file))
     } else {
