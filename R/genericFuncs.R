7f83ae0548f1b938e1aacbe56d28e264ad204ca9
David Zhang <j.zhang@dkfz.de>
2008-06-25 12:33:32 +0000

    New functions: replace methods for interactors, , taxId, organismName and releaseDate added (all for psimi25InteractionEntry objects)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RpsiXML@32417 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genericFuncs.R
+++ R/genericFuncs.R
@@ -1,5 +1,7 @@
 ## interactors
 setGeneric("interactors", function(x) standardGeneric("interactors"))
+setGeneric("interactors<-", function(x, value) standardGeneric("interactors<-"))
+
 setGeneric("availableXrefs", function(x,...) standardGeneric("availableXrefs"))
 
 setGeneric("translateID",
@@ -8,6 +10,7 @@ setGeneric("sourceDb",function(x) standardGeneric("sourceDb"))
 setGeneric("sourceId",function(x) standardGeneric("sourceId"))
 
 setGeneric("interactions", function(x) standardGeneric("interactions"))
+setGeneric("interactions<-", function(x, value) standardGeneric("interactions<-"))
 setGeneric("complexes", function(x) standardGeneric("complexes"))
 setGeneric("members", function(x) standardGeneric("members"))
 setGeneric("fullName", function(x) standardGeneric("fullName"))
@@ -15,8 +18,14 @@ setGeneric("fullName", function(x) standardGeneric("fullName"))
 setGeneric("confidenceValue", function(x) standardGeneric("confidenceValue"))
 setGeneric("interactionType", function(x) standardGeneric("interactionType"))
 setGeneric("organismName", function(x) standardGeneric("organismName"))
+setGeneric("organismName<-", function(x, value) standardGeneric("organismName<-"))
+
 setGeneric("taxId", function(x) standardGeneric("taxId"))
+setGeneric("taxId<-", function(x, value) standardGeneric("taxId<-"))
+
 setGeneric("releaseDate", function(x) standardGeneric("releaseDate"))
+setGeneric("releaseDate<-", function(x, value) standardGeneric("releaseDate<-"))
+
 setGeneric("interactionType", function(x) standardGeneric("interactionType"))
 setGeneric("interactorInfo", function(x) standardGeneric("interactorInfo"))
 
