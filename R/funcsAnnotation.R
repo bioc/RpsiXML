943f381124bb68d05815a3e9c0918c5acfe12fe8
Jitao David Zhang <jitao_david.zhang@roche.com>
2021-05-04 16:33:31 +0200

    add ignore, and fix import/export issues

--- R/funcsAnnotation.R
+++ R/funcsAnnotation.R
@@ -1,13 +1,11 @@
 getAbstractByPMID <- function(pmID){
-
-pmXML <- pubmed(pmID)
-pmRoot <- xmlRoot(pmXML)
-numAbst <- length(pmRoot)
-abstracts <- vector("list", length=numAbst)
-for(i in 1:numAbst){
-  abstracts[[i]] <- buildPubMedAbst(pmRoot[[i]])
-}
-names(abstracts) <- pmID
-return(abstracts)
-
+	pmXML <- annotate::pubmed(pmID)
+	pmRoot <- xmlRoot(pmXML)
+	numAbst <- length(pmRoot)
+	abstracts <- vector("list", length=numAbst)
+	for(i in 1:numAbst){
+	  abstracts[[i]] <- buildPubMedAbst(pmRoot[[i]])
+	}
+	names(abstracts) <- pmID
+	return(abstracts)
 }
